86f56499b993c3f59613e74697094811a2ec2a3c
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-04-21 10:07:15 -0700

    User cookies

--- src/Component/Context/User.jsx
+++ src/Component/Context/User.jsx
@@ -1,4 +1,27 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
+import { getCookie, setCookie, hasCookie, deleteCookie } from "cookies-next";
+
+const setUserCookie = newCookieUser => {
+  const date = new Date();
+  date.setTime(date.getTime() + 60 * 60 * 1000);
+  setCookie("userName", newCookieUser.name, { expires: date });
+  setCookie("userId", newCookieUser.id, { expires: date });
+};
+
+const getUserCookie = () => {
+  if (hasCookie("userName")) {
+    const userName = getCookie("userName");
+    const userId = getCookie("userId");
+    return { name: userName, id: userId };
+  } else {
+    return {};
+  }
+};
+
+const clearUserCookie = () => {
+  deleteCookie("userName");
+  deleteCookie("userId");
+};
 
 const UserContext = createContext({
   user: {},
@@ -8,6 +31,10 @@ const UserContext = createContext({
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(getUserCookie());
+  }, [setUser]);
 
   const logIn = async user => {
     const res = await fetch(`api/user/login/`, {
@@ -20,6 +47,7 @@ export const UserContextProvider = ({ children }) => {
     if (res.status === 200) {
       const { user } = await res.json();
       setUser(user);
+      setUserCookie(user);
     } else {
       const { error } = await res.json();
       console.error(error);
@@ -28,6 +56,7 @@ export const UserContextProvider = ({ children }) => {
 
   const logOut = () => {
     setUser({});
+    clearUserCookie();
   };
 
   return <UserContext.Provider value={{ user, logIn, logOut }}>{children}</UserContext.Provider>;
